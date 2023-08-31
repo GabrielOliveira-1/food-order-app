48f9aaa08334a78f06601f49df5bde52f6869e5c
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-08-31 09:33:04 -0600

    Update food list style
    - Add MealItem.js, Card.js amd its respective CSS modules
    
    - Add MealItemform template to implement later
    
    - Implement MealItem and AvailableMeals logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import MealItem from "./Components/Meals/MealItem/MealItem";
 
 function App() {
   return (
