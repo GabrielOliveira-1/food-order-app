07cef29263abd3a3154e701ff142a94d12c23fa4
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-09-03 19:25:00 -0600

    Implement Modal logic
    - Improve logic to show and hide Cart
    
    - Update Dummy data to fit general app theme

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>HealthyMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Delicious food!" />
