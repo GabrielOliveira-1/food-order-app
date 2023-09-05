8b8fbd5d2f2c866cdc8c7397a12de731b78cdeaa
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-09-05 08:46:02 -0600

    Update CartProvider.js
    - Add useReducer to handle actions in cart

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -1,8 +1,37 @@
+import { useReducer } from "react";
 import CartContext from "./cart-content";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHadler = (item) => {};
-  const removeItemToCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHadler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemToCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
     items: [],
