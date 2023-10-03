8d8da5442bdfb83ac761105f5171e8ebd5a58485
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-10-03 14:57:03 -0600

    Update AvailableMeals component and CSS module
    - Implement Loading State for when fetching meals
    
    - Implement error handling for when fetching data

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -33,12 +33,19 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-c9aa4-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong.");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -53,10 +60,22 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>Loading...</section>;
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>{httpError}</section>;
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
